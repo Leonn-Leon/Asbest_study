51a0eda28c6377b23586d3bc1c4f658d3abc6b9a
Vitaly <skovoroda.vitalik@yandex.ru>
2023-02-19 21:08:54 +0500

    RandomCrop, изменил аугментацию

--- train_model.py
+++ train_model.py
@@ -5,28 +5,48 @@ import segmentation_models_pytorch as smp
 os.environ["CUDA_VISIBLE_DEVICES"]="2"
 
 path = Path('/storage/vskovoroda/Stones/data')
-dls = SegmentationDataLoaders.from_label_func(path = path/'images',
-            bs = 4,
-            batch_tfms=[Normalize.from_stats(*imagenet_stats), aug_transforms],
+dls = SegmentationDataLoaders.from_label_func(path = path,
+            bs = 2,
+            item_tfms = RandomCrop(480),
+            batch_tfms=[Normalize.from_stats(*imagenet_stats), 
+                        *aug_transforms(
+                            mult=1.0,
+                            do_flip=True,
+                            flip_vert=True,
+                            max_rotate=45.0,
+                            max_lighting=0.0,
+                            max_warp=0.0,
+                            p_affine=0.8,
+                            pad_mode=PadMode.Zeros
+                    )],
             fnames = get_image_files(path/'images'),
             label_func = lambda o: path/'annotations'/o.name,
             codes = ['no_asbest', 'asbest'])
 
 print('DLS create!')
 
-model = smp.DeepLabV3Plus(
-    encoder_name="resnet34", 
+model = smp.UnetPlusPlus(
+    encoder_name="resnext101_32x48d", 
     encoder_weights=None,    
-    activation = 'sigmoid',
-    in_channels=3,           
+    in_channels=3,    
+    activation = 'sigmoid',       
     classes=1,                
 )
 
-learn = Learner(dls, model, loss_func=DiceLoss())
+learn = Learner(dls, model, loss_func=DiceLoss(),
+                metrics=[smp.losses.TverskyLoss(mode='binary'),
+                     smp.losses.JaccardLoss(mode='binary'),
+                     smp.losses.FocalLoss(mode='binary'),
+                     smp.losses.LovaszLoss(mode='binary'),
+                     smp.losses.MCCLoss()
+                    ])
 
-print('Start fit DeepLabV3Plus')
-learn.fit(7)
+print('Start fit UnetPlusPlus')
+learn.fit(5)
 
-learn.save('DeepLabV3Plus')
+learn.save('UnetPlusPlus')
 
-print('DeepLabV3Plus Done!')
+print('Start fine_tune')
+learn.fine_tune(2)
+
+print('UnetPlusPlus Done!')
--- train_model_2.py
+++ train_model_2.py
@@ -2,31 +2,44 @@ import os
 from fastai.vision.all import *
 import segmentation_models_pytorch as smp
 
-os.environ["CUDA_VISIBLE_DEVICES"]="3"
+os.environ["CUDA_VISIBLE_DEVICES"]="2"
 
 path = Path('/storage/vskovoroda/Stones/data')
 dls = SegmentationDataLoaders.from_label_func(path = path,
-            bs = 1,
-            batch_tfms=[Normalize.from_stats(*imagenet_stats), aug_transforms],
+            bs = 5,
+            item_tfms = RandomCrop(480),
+            batch_tfms=[Normalize.from_stats(*imagenet_stats), 
+                        *aug_transforms(
+                            mult=1.0,
+                            do_flip=True,
+                            flip_vert=True,
+                            max_rotate=45.0,
+                            max_lighting=0.0,
+                            max_warp=0.0,
+                            p_affine=0.8,
+                            max_zoom = 1.0,
+                            pad_mode=PadMode.Zeros
+                    )],
             fnames = get_image_files(path/'images'),
             label_func = lambda o: path/'annotations'/o.name,
             codes = ['no_asbest', 'asbest'])
 
 print('DLS create!')
 
-model = smp.UnetPlusPlus(
-    encoder_name="resnet34", 
-    encoder_weights=None,    
-    in_channels=3,
-    activation = 'sigmoid',           
-    classes=1,                
-)
+# model = smp.UnetPlusPlus(
+#     encoder_name="resnet152", 
+#     encoder_weights=None,    
+#     in_channels=3,
+#     activation = 'sigmoid',           
+#     classes=1,                
+# )
 
-learn = Learner(dls, model, loss_func=DiceLoss())
+learn = unet_learner(dls, resnet152, loss_func=DiceLoss(), y_range=(0,1),
+                pretrained = False)
 
-print('Start fit UnetPlusPlus')
+print('Start fit unet_learner_res152')
 learn.fit(7)
 
-learn.save('UnetPlusPlus')
+learn.save('unet_learner_res152')
 
-print('UnetPlusPlus Done!')
+print('unet_learner_res152 Done!')
